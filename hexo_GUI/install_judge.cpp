13c3590488afdc4f41c44e191ce25a699c0ec799
intmian <1437131326@qq.com>
2018-07-11 19:44:06 +0800

    完善了一些类

--- hexo_GUI/install_judge.cpp
+++ hexo_GUI/install_judge.cpp
@@ -1,1 +1,53 @@
-﻿//todo
+﻿#include "install_judge.h"
+#include <Windows.h>//TO BE improve
+using namespace std;
+bool installer::install_node(std::string place)
+{
+	return false;
+}
+
+bool installer::install_git(std::string place)
+{
+	return false;
+}
+
+bool installer::search(std::string place)
+{
+	return false;
+}
+
+bool installer::start()
+{
+	if (!search(bin_place))
+	{
+		MessageBoxA(NULL, "未找到完整的安装文件", "安装被终止", MB_OK);
+		return false;
+	}
+	string temp;
+	temp = bin_place + "node";//to change
+	if (install_node(temp))
+	{
+		temp = bin_place + "git";//to change
+		if (!install_git(temp))
+		{
+			MessageBoxA(NULL,"安装git失败，请重新下载hexo_GUI","安装被终止",MB_OK);
+			return false;
+		}
+	}
+	else
+	{
+		MessageBoxA(NULL, "安装node_js失败，请重新下载hexo_GUI", "安装被终止", MB_OK);
+		return false;
+	}
+	return true;
+}
+
+bool installer::startProcess(std::string place)
+{
+	_wsystem(place.data());
+}
+
+installer::installer(std::string place):
+	bin_place(place)
+{
+}
--- hexo_GUI/install_judge.h
+++ hexo_GUI/install_judge.h
@@ -8,7 +8,6 @@
 #include <string>
 #endif // !_STRING
 
-
 #ifndef _CLASS_installer
 #define _CLASS_installer
 class installer
@@ -16,9 +15,11 @@ class installer
 private:
 	bool install_node(std::string place);//安装node.js place即node js地址
 	bool install_git(std::string place);//安装git place即git地址
+	bool startProcess(std::string place);
 	bool search(std::string place);
 	std::string bin_place;
 public:
+	bool start();
 	installer(std::string place);//place是exe的上一层文件夹
 };
 #endif // !_CLASS_installer
