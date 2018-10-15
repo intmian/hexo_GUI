6772dfa50c4fa1597c5719711853f0d5dc66cbab
intmian <1437131326@qq.com>
2018-10-15 12:34:45 +0800

    auto

--- hexo_GUI/Tool.h
+++ hexo_GUI/Tool.h
@@ -25,8 +25,3 @@ public:
 	  输入得非二按钮数量全被视为1*/
 	static bool message_box(message_Type type, const char *title, const char *contain, int button_number = 1);//只能使用一个或两个按钮
 };
-class Run_code
-{
-private:
-	FILE * PPipe;
-};
